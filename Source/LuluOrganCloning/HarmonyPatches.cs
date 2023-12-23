d239cf2120f4b84b35e318c7176c19ad6a28485f
Cerespirin <151862361+Cerespirin@users.noreply.github.com>
2023-12-23 08:24:28 -0500

    IDE0055

--- Source/LuluOrganCloning/HarmonyPatches.cs
+++ Source/LuluOrganCloning/HarmonyPatches.cs
@@ -7,30 +7,30 @@ using Verse;
 
 namespace LoonyLadle.OrganCloning
 {
-    [StaticConstructorOnStartup]
-    static class HarmonyPatches
-    {
-        static HarmonyPatches()
-        {
-            Harmony harmony = new Harmony("rimworld.loonyladle.organcloning");
+	[StaticConstructorOnStartup]
+	static class HarmonyPatches
+	{
+		static HarmonyPatches()
+		{
+			Harmony harmony = new Harmony("rimworld.loonyladle.organcloning");
 
-            MethodInfo method1 = AccessTools.Method(typeof(PawnUtility), "TrySpawnHatchedOrBornPawn");
-            HarmonyMethod patch1 = new HarmonyMethod(typeof(HarmonyPatches), nameof(AddClonedOrgans));
-            harmony.Patch(method1, null, patch1);
-        }
-        
-        public static void AddClonedOrgans(Pawn pawn, Thing motherOrEgg)
-        {
-            if (motherOrEgg is Pawn mother)
-            {
-                HediffComp_OrganProps organProps = mother.health.hediffSet.GetFirstHediffOfDef(HediffDefOf.Pregnant).TryGetComp<HediffComp_OrganProps>();
+			MethodInfo method1 = AccessTools.Method(typeof(PawnUtility), "TrySpawnHatchedOrBornPawn");
+			HarmonyMethod patch1 = new HarmonyMethod(typeof(HarmonyPatches), nameof(AddClonedOrgans));
+			harmony.Patch(method1, null, patch1);
+		}
 
-                foreach (BodyPartRecord part in organProps.organsToClone)
-                {
-                    Hediff hediff = HediffMaker.MakeHediff(MyDefOf.LuluOrganCloning_ClonedOrgan, pawn, part);
-                    pawn.health.AddHediff(hediff);
-                }
-            }
-        }
-    }
+		public static void AddClonedOrgans(Pawn pawn, Thing motherOrEgg)
+		{
+			if (motherOrEgg is Pawn mother)
+			{
+				HediffComp_OrganProps organProps = mother.health.hediffSet.GetFirstHediffOfDef(HediffDefOf.Pregnant).TryGetComp<HediffComp_OrganProps>();
+
+				foreach (BodyPartRecord part in organProps.organsToClone)
+				{
+					Hediff hediff = HediffMaker.MakeHediff(MyDefOf.LuluOrganCloning_ClonedOrgan, pawn, part);
+					pawn.health.AddHediff(hediff);
+				}
+			}
+		}
+	}
 }
--- Source/LuluOrganCloning/Recipe_PrepareClonedOrgan.cs
+++ Source/LuluOrganCloning/Recipe_PrepareClonedOrgan.cs
@@ -8,56 +8,56 @@ using Verse;
 
 namespace LoonyLadle.OrganCloning
 {
-    public class Recipe_PrepareClonedOrgan : Recipe_Surgery
-    {
-        public override IEnumerable<BodyPartRecord> GetPartsToApplyOn(Pawn pawn, RecipeDef recipe)
-        {
-            // Get the pawn's pregnancy.
-            Hediff pregnancy = pawn.health.hediffSet.GetFirstHediffOfDef(HediffDefOf.Pregnant);
-            
-            // Only perform this operation on early, visible pregnancies.
-            if (pregnancy == null || !pregnancy.Visible || pregnancy.CurStageIndex > 1)
-            {
-                // Return empty result; the things that call this method don't perform null checking.
-                return Enumerable.Empty<BodyPartRecord>();
-            }
+	public class Recipe_PrepareClonedOrgan : Recipe_Surgery
+	{
+		public override IEnumerable<BodyPartRecord> GetPartsToApplyOn(Pawn pawn, RecipeDef recipe)
+		{
+			// Get the pawn's pregnancy.
+			Hediff pregnancy = pawn.health.hediffSet.GetFirstHediffOfDef(HediffDefOf.Pregnant);
 
-            // Get our organProps.
-            HediffComp_OrganProps organProps = pregnancy.TryGetComp<HediffComp_OrganProps>();
+			// Only perform this operation on early, visible pregnancies.
+			if (pregnancy == null || !pregnancy.Visible || pregnancy.CurStageIndex > 1)
+			{
+				// Return empty result; the things that call this method don't perform null checking.
+				return Enumerable.Empty<BodyPartRecord>();
+			}
 
-            // Return all parts that spawn things on removed which are not recorded in our organProps.
-            return pawn.def.race.body.AllParts.Where(p => p.def.spawnThingOnRemoved != null && !organProps.organsToClone.Contains(p));
-        }
+			// Get our organProps.
+			HediffComp_OrganProps organProps = pregnancy.TryGetComp<HediffComp_OrganProps>();
 
-        public override void ApplyOnPawn(Pawn pawn, BodyPartRecord part, Pawn billDoer, List<Thing> ingredients, Bill bill)
-        {
-            // PawnDoer stuff, copied from Recipe_RemoveBodyPart.
-            if (billDoer != null)
-            {
-                if (CheckSurgeryFail(billDoer, pawn, ingredients, part, bill))
-                {
-                    return;
-                }
-                TaleRecorder.RecordTale(TaleDefOf.DidSurgery, new object[]
-                {
-                    billDoer,
-                    pawn
-                });
-            }
-            
-            // Record the organ to be cloned in our organProps.
-            pawn.health.hediffSet.GetFirstHediffOfDef(HediffDefOf.Pregnant).TryGetComp<HediffComp_OrganProps>().organsToClone.Add(part);
+			// Return all parts that spawn things on removed which are not recorded in our organProps.
+			return pawn.def.race.body.AllParts.Where(p => p.def.spawnThingOnRemoved != null && !organProps.organsToClone.Contains(p));
+		}
 
-            // Faction stuff, copied from Recipe_RemoveBodyPart.
-            if (IsViolationOnPawn(pawn, part, Faction.OfPlayer) && pawn.Faction != null && billDoer != null && billDoer.Faction != null)
-            {
-                Faction faction = pawn.Faction;
-                Faction faction2 = billDoer.Faction;
-                int goodwillChange = -15;
-                //string reason = "GoodwillChangedReason_RemovedBodyPart".Translate(part.LabelShort);
-                GlobalTargetInfo? lookTarget = new GlobalTargetInfo?(pawn);
-                faction.TryAffectGoodwillWith(faction2, goodwillChange, true, true, null, lookTarget);
-            }
-        }
-    }
+		public override void ApplyOnPawn(Pawn pawn, BodyPartRecord part, Pawn billDoer, List<Thing> ingredients, Bill bill)
+		{
+			// PawnDoer stuff, copied from Recipe_RemoveBodyPart.
+			if (billDoer != null)
+			{
+				if (CheckSurgeryFail(billDoer, pawn, ingredients, part, bill))
+				{
+					return;
+				}
+				TaleRecorder.RecordTale(TaleDefOf.DidSurgery, new object[]
+				{
+						  billDoer,
+						  pawn
+				});
+			}
+
+			// Record the organ to be cloned in our organProps.
+			pawn.health.hediffSet.GetFirstHediffOfDef(HediffDefOf.Pregnant).TryGetComp<HediffComp_OrganProps>().organsToClone.Add(part);
+
+			// Faction stuff, copied from Recipe_RemoveBodyPart.
+			if (IsViolationOnPawn(pawn, part, Faction.OfPlayer) && pawn.Faction != null && billDoer != null && billDoer.Faction != null)
+			{
+				Faction faction = pawn.Faction;
+				Faction faction2 = billDoer.Faction;
+				int goodwillChange = -15;
+				//string reason = "GoodwillChangedReason_RemovedBodyPart".Translate(part.LabelShort);
+				GlobalTargetInfo? lookTarget = new GlobalTargetInfo?(pawn);
+				faction.TryAffectGoodwillWith(faction2, goodwillChange, true, true, null, lookTarget);
+			}
+		}
+	}
 }
